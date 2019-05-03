2479d32970d22bef6d5485739eceb883f182b46c
Tom Gray <tom.gray.ext@bbc.co.uk>
2019-05-03 11:09:53 +0100

    ignore dist and do a test file to test the build

--- src/build.test.js
+++ src/build.test.js
@@ -6,6 +6,6 @@ describe('build test', () => {
   test('ensure LRUD build is in correct format', () => {
     const tree = new Lrud()
 
-    console.log('tree', tree.registerNode)
+    expect(tree.registerNode).not.toBeUndefined()
   })
 })
