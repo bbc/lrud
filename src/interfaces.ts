6ece282ff0a152cbdf363972019876f83b0f3ce8
Tom Gray <tom.gray.ext@bbc.co.uk>
2019-06-05 13:10:49 +0100

    wiring up types

---
+++ src/interfaces.ts
@@ -0,0 +1,22 @@
+export interface Node {
+    id: string;
+    parent?: string;
+    parents?: string;
+    index?: number;
+    indexRange?: number[];
+    selectAction?: Function;
+    isFocusable?: boolean;
+    isWrapping?: boolean;
+    orientation?: string;
+    isIndexAlign?: boolean;
+    onLeave?: Function;
+    onEnter?: Function;
+    activeChild?: string;
+    children?: any;
+}
+
+export interface Override {
+    id: string;
+    direction: string;
+    target: string;
+}
