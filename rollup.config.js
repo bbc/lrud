28231e46b37556d64183f2187f64b07cd9d14b55
Murray Coghill <murray.coghill@gmail.com>
2019-10-23 13:56:29 +0100

    Use rollup for both build processes

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,41 @@
 import typescriptPlugin from 'rollup-plugin-typescript2'
-import pkg from './package.json'
 import nodeResolve from 'rollup-plugin-node-resolve'
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
+import { uglify } from 'rollup-plugin-uglify'
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
       file: 'dist/cjs/index.js',
       format: 'cjs'
-    }
-  ],
-  external: [
-    ...Object.keys(pkg.peerDependencies || {})
-  ],
-  plugins: [
-    typescriptPlugin({
-      useTsconfigDeclarationDir: true
-    }),
-    nodeResolve()
-  ]
-}
+    },
+    plugins: [
+      typescriptPlugin({
+        useTsconfigDeclarationDir: true
+      }),
+      nodeResolve()
+    ]
+  },
+  {
+    input: 'dist/cjs/index.js',
+    output: {
+      file: 'dist/cjs/index.min.js',
+      format: 'cjs'
+    },
+    plugins: [
+      uglify()
+    ]
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/esm/index.js',
+      format: 'esm'
+    },
+    plugins: [
+      typescriptPlugin({
+        useTsconfigDeclarationDir: true
+      })
+    ]
+  }
+]
